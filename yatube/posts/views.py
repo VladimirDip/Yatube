1e4f734062ee7519d83102fc2e5557ca83b20d0a
VladimirDip <kent4594111@gmail.com>
2021-12-24 08:08:11 +1000

    started create 'homepage'

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:10]
+    return render(request, "index.html", {"posts": latest})
 # Create your views here.
